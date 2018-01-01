5ce1c7f835805db54cf510854244226c2b6efc2d
Akshay Kumar Gupta <akshaygupta@drishti-soft.com>
2018-01-02 02:55:12 +0530

    Initial commit

---
+++ app/src/main/java/com/greentour/bike/application/MainActivity.java
@@ -0,0 +1,183 @@
+package com.greentour.bike.application;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Array of strings storing country names
+    String[] countries = new String[]{
+            "India",
+            "Pakistan",
+            "Sri Lanka",
+            "China",
+            "Bangladesh",
+            "Nepal",
+            "Afghanistan",
+            "North Korea",
+            "South Korea",
+            "Japan"
+    };
+
+    // Array of integers points to images stored in /res/drawable-ldpi/
+    int[] flags = new int[]{
+            R.drawable.bike1,
+            R.drawable.bike2,
+            R.drawable.bike3,
+            R.drawable.bike4,
+            R.drawable.bike5,
+            R.drawable.bike6,
+            R.drawable.bike7,
+            R.drawable.bike8,
+            R.drawable.bike9,
+            R.drawable.bike10
+    };
+
+    // Array of strings to store currencies
+    String[] currency = new String[]{
+            "Indian Rupee",
+            "Pakistani Rupee",
+            "Sri Lankan Rupee",
+            "Renminbi",
+            "Bangladeshi Taka",
+            "Nepalese Rupee",
+            "Afghani",
+            "North Korean Won",
+            "South Korean Won",
+            "Japanese Yen"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "AKSHAY", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        this.populateBikes();
+
+    }
+
+    private void populateBikes() {
+        // Each row in the list stores country name, currency and flag
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("bikePrice", "Country : " + countries[i]);
+            hm.put("bikeName", "Currency : " + currency[i]);
+            hm.put("bikeIcon", Integer.toString(flags[i]));
+            aList.add(hm);
+        }
+
+        // Keys used in Hashmap
+        String[] from = {"bikeIcon", "bikeName", "bikePrices"};
+
+        // Ids of views in listview_layout
+        int[] to = {R.id.bikeIcon, R.id.bikeName, R.id.bikePrice};
+
+        // Instantiating an adapter to store each items
+        // R.layout.listview_layout defines the layout of each item
+        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), aList, R.layout.bike_list_layout, from, to);
+
+        // Getting a reference to listview of main.xml layout file
+        ListView listView = (ListView) findViewById(R.id.bike_list);
+
+        // Setting the adapter to the listView
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
